b35c065768d9f10a1ef5bbe8f49264011f9d3193
Batool Fatima <batoolfatima2135@gmail.com>
2023-08-11 23:58:07 +0500

    Add configuration for images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['fakestoreapi.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'fakestoreapi.com',
+        port: '',
+        pathname: '/img/.*',
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
